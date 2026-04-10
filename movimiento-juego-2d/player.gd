b99acb6fbce908b2dca89c4b6614ce018fb6f408
GonzaloDP <gondap@outlook.com>
2026-04-10 13:44:43 -0300

    Salto

--- movimiento-juego-2d/player.gd
+++ movimiento-juego-2d/player.gd
@@ -1,32 +1,28 @@
 extends CharacterBody2D
 
-
 const velocidad = 5000
-const velocidad_salto = -1000
-var gravedad: float
+const gravedad = 9
+const dash = 500000
 var salto_diponible = true
+var velocidad_de_salto = -500
 
 func _process(delta: float) -> void:
-	
 	pass
 
 func _physics_process(delta: float) -> void:
-	var direction = Input.get_axis("izquierda","derecha")
-	velocity.x = direction * velocidad * delta
+	var direction = Input.get_axis("izquierda","derecha") #Variable de dirección a donde camina
+	velocity.x = direction * velocidad * delta      #Mueve en el eje X
+	if Input.is_action_just_pressed("dash"):
+		velocity.x = direction * (dash * delta)
 	
-	if Input.is_action_just_pressed("saltar") and salto_diponible:
-		gravedad = -200.0
-		velocity.y = velocidad_salto * delta
-		
 	if is_on_floor():
 		salto_diponible = true
-		gravedad = 0.0
 	else:
 		salto_diponible = false
-		velocity.y = gravedad + 5500.0 * delta
 		
-	
-	
+	if Input.is_action_just_pressed("saltar") and salto_diponible:
+		velocity.y += velocidad_de_salto
+	velocity.y += gravedad
 	
 	move_and_slide()
 	
