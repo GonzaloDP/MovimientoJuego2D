28febb85ce42da25c33ce494ba6e05c611c5a471
GonzaloDP <gondap@outlook.com>
2026-04-12 11:42:57 -0300

    parry

--- movimiento-juego-2d/player.gd
+++ movimiento-juego-2d/player.gd
@@ -9,6 +9,8 @@ var salto_diponible = true
 var velocidad_de_salto = -500.0
 var dash_direccion: float
 var dasheando: bool
+var se_puede_dashear = true
+var puede_parrear: bool
 
 func _process(delta: float) -> void:
 	pass
@@ -17,14 +19,16 @@ func _physics_process(delta: float) -> void:
 	var direction = Input.get_axis("izquierda","derecha") #Variable de dirección a donde camina
 	velocity.x = direction * velocidad * delta      #Mueve en el eje X
 	
-	if Input.is_action_just_pressed("dash"):
+	if Input.is_action_just_pressed("dash") and se_puede_dashear: #Dash del personaje
 		dash_direccion = direction
 		DashDuracion.start()
 		dasheando = true
-	if dasheando:
+	if dasheando:  #Dasheando con el pj
 		velocity.x = dash_direccion * dash
+		velocity.y = 0.0
+		se_puede_dashear = false
 	
-	if is_on_floor(): #Condición del salto
+	if is_on_floor() or puede_parrear: #Condición del salto
 		salto_diponible = true
 	else:
 		salto_diponible = false
@@ -35,7 +39,12 @@ func _physics_process(delta: float) -> void:
 	
 	move_and_slide()
 	
-
-func _on_dash_duracion_timeout() -> void:
+func _on_dash_duracion_timeout() -> void: #Señal para saber que hacer cuando termina el timer del dash
 	dasheando = false
 	DashDuracion.stop()
+	se_puede_dashear = true
+
+func _on_objeto_parreable_body_entered(body: Node2D) -> void:
+	puede_parrear = true
+func _on_objeto_parreable_body_exited(body: Node2D) -> void:
+	puede_parrear = false
